38d556cf4930fd76a69d69c2c683e3b969db597b
Ashadeep Pan <116565915+Ashadeep2002@users.noreply.github.com>
2026-08-18 01:52:18 +0530

    Small change in metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Elevateaxis - Bringing technology to life",
+  title: "Elevatexis - Bringing technology to life",
   description: "Making digital experiences sound human",
 };
 
